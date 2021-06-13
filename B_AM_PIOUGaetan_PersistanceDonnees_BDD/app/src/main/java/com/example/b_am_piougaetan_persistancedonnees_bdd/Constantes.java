e251f454689c5a4dbf6f8b8eb175ff9973432909
Gaetan-Piou <44892423+Gaetan-Piou@users.noreply.github.com>
2021-06-13 23:26:51 +0200

    Version

---
+++ B_AM_PIOUGaetan_PersistanceDonnees_BDD/app/src/main/java/com/example/b_am_piougaetan_persistancedonnees_bdd/Constantes.java
@@ -0,0 +1,12 @@
+package com.example.b_am_piougaetan_persistancedonnees_bdd;
+
+public class Constantes {
+    private static final String root_URL = "http://192.168.144.53/AndroidPHPMySQL/V1/";
+    public static String URL_Register = root_URL+"UserRegister.php";
+
+    public static String URL_Login = root_URL+"UserList.php";
+
+    public static String URL_Search = root_URL+"UserSearch.php";
+
+    public static String URL_Delete = root_URL+"UserDelete.php";
+}
